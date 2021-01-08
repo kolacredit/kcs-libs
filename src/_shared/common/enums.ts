2acb7dd5bb833358731329711b2c39a1d4514521
nondefyde <nondefyde@gmail.com>
2021-01-08 14:58:22 +0100

    updated libraray for other services use

--- src/_shared/common/enums.ts
+++ src/_shared/common/enums.ts
@@ -1,12 +1,12 @@
 export enum QueueTasks {
-  UPLOAD_PHOTO = 'task.upload.photo',
-  SEND_SMS = 'task.send.sms',
-  SEND_EMAIL = 'task.send.email',
-  PING = 'task.send.ping',
+  UPLOAD_PHOTO = 'kc.task.upload.photo',
+  SEND_SMS = 'kc.task.send.sms',
+  SEND_EMAIL = 'kc.task.send.email',
+  PING = 'kc.task.send.ping',
 }
 
 export enum WorkerQueue {
-  PROCESS_WORK = 'wevied.jobs.process.work',
+  PROCESS_WORK = 'kc.wevied.jobs.process.work',
 }
 
 export enum AppStatus {
--- src/_shared/services/index.ts
+++ src/_shared/services/index.ts
@@ -2,4 +2,3 @@ export * from './jwt.strategy';
 export * from './file-upload.service';
 export * from './job.service';
 export * from './mail.service';
-export * from './paystack.service';
