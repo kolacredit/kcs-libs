d798676bcf669be315983a389f77f90c87c4d64a
nondefyde <nondefyde@gmail.com>
2021-02-25 15:57:19 +0100

    fixed deletion updated

--- src/todo/dto/CreateTodoDto.ts
+++ src/todo/dto/CreateTodoDto.ts
@@ -1,15 +1,12 @@
-import { Transform } from 'class-transformer';
 import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTodoDto {
   @ApiProperty({ example: 'Task 1' })
   @IsString()
-  @Transform(s => s.trim().toLowerCase())
   readonly title: string;
 
   @ApiProperty({ example: '3820' })
   @IsString()
-  @Transform(s => s.trim())
   readonly description: string;
 }
--- src/todo/dto/UpdateTodoDto.ts
+++ src/todo/dto/UpdateTodoDto.ts
@@ -1,9 +1,7 @@
-import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTodoDto {
   @ApiProperty({ example: 'Task 1' })
-  @Transform(s => s.trim().toLowerCase())
   title: string;
 
   @ApiProperty({ example: 'The todo description' })
