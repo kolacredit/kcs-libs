d396c504f4381aaeda46174fb59f3651d2da1ead
nondefyde <nondefyde@gmail.com>
2021-03-21 15:42:50 +0100

    added getter to report job

--- src/_shared/jobs/index.ts
+++ src/_shared/jobs/index.ts
@@ -1,3 +1,4 @@
 export * from './email.job';
 export * from './sms.job';
+export * from './report.job';
 export * from './job';
--- src/_shared/jobs/report.job.ts
+++ src/_shared/jobs/report.job.ts
@@ -5,38 +5,38 @@ import { WorkerQueue } from '../common';
 import { Job } from './job';
 
 export class ReportJob extends Job {
-  private _bvn: string;
-  private _user: string;
-  private _reportType: string;
+  private bvn: string;
+  private user: string;
+  private reportType: string;
 
   constructor() {
     super(WorkerQueue.PROCESS_WORK);
   }
 
-  get bvn() {
-    return this._bvn;
+  public getBvn() {
+    return this.bvn;
   }
 
-  get user() {
-    return this._user;
+  public getUser() {
+    return this.user;
   }
 
-  get reportType() {
-    return this._reportType;
+  get getReportType() {
+    return this.reportType;
   }
 
   public setBvn(user: string) {
-    this._bvn = user;
+    this.bvn = user;
     return this;
   }
 
   public setUser(user: string) {
-    this._user = user;
+    this.user = user;
     return this;
   }
 
   public setReportType(reportType: string) {
-    this._reportType = reportType;
+    this.reportType = reportType;
     return this;
   }
 }
